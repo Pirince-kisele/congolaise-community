c0cb77719aeaba5341fde48c70c108f4cf1a2049
Pirince-kisele <156867185+Pirince-kisele@users.noreply.github.com>
2024-04-02 07:39:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true })
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.get('/', (req, res) =>{
+  res.render('pages/index');
+})
+app.get('/about', (req, res) =>{
+  res.render('pages/about');
+})
+app.get('/contact', (req, res) =>{
+  res.render('pages/contact');
+})
+app.get('/news', (req, res) =>{
+  res.render('pages/news');
+})
+app.get('/question', (req, res) =>{
+  res.render('pages/question');
+})
+app.get('/notFound', (req, res) =>{
+  res.render('pages/notFound');
+})
+app.get('/post', (req, res) =>{
+  res.render('pages/post');
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
